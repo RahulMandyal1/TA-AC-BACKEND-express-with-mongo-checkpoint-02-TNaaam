206bae69df3062388dbe96d653b888effdeba9eb
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-04-03 23:24:33 +0530

    updated checkpoint

--- block-BNaaft/eventmanagement/app.js
+++ block-BNaaft/eventmanagement/app.js
@@ -33,6 +33,7 @@ app.use("/", indexRouter);
 app.use("/events", eventRouter);
 app.use("/events", remarksRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
